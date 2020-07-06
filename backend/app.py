bb4939b0090f03ca110c3dfaed93e3d42bc26a3f
Jasiek <janek9805@gmail.com>
2020-07-06 02:40:39 +0200

    backend/app.py modified

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,38 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import json
 
-with open('data.json') as file:
+"""
+with open('./data.json') as file:
     data = json.load(file)
-
+"""
 app = Flask(__name__)
 CORS(app)
-data_base = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postdb@localhost/nti'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True)
+    email = db.Column(db.String(100), unique = True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+#examples = Example.query.all()
+#for ex in examples:
+#    print(ex.data)
+db.create_all()
+db.session.commit()
+users = User.query.all()
+print(users)
+"""
 @app.route('/')
 def hello_world():
     return 'Hello world!'
@@ -52,7 +77,7 @@ def add_comment():
 @app.route('/rev/<float:revNo>')
 def revision(revNo):
     return 'Revision number %f' % revNo
-
+"""
 if __name__ == '__main__':
     #app.run(debug = True)
     app.run(debug= True, host='127.0.0.1', port=5000)
